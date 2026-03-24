302814f5b0af855643ddea0ce152a7ff9cb24a15
giorgikh93 <giorgikhatiashvili93@yahoo.com>
2026-03-24 19:08:12 +0400

    restore actual link

--- appling/app.cjs
+++ appling/app.cjs
@@ -3,4 +3,4 @@ const { install } = require('./lib/install')
 // When we have pear://pearpass, use this
 // install("pearpass");
 
-install('bga1jdn1o8u5wqop5a7dwahohfj4s5dkui6jbk75dab7d7r1k4zo')
+install('tywsat7gz8m65ejx4zjn3773pbdc4j8m66tukis8dgzekraymtzo')
